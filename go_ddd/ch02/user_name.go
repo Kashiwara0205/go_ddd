6780b13bf00e5f7a12bc6d2aa71df019823a5ce1
kashiwara0205 <tamatebako0205@gmail.com>
2022-10-23 15:40:14 +0900

    Add user_name value object

--- go_ddd/ch02/user_name.go
+++ go_ddd/ch02/user_name.go
@@ -1,11 +1,26 @@
 package ch02
 
+import (
+    "errors"
+	"fmt"
+)
+
 type UserName struct{
-	value string
+	value *string
 }
 
-func NewUserName(value string) UserName{
-	return UserName{
-		value: value,
+func NewUserName(value *string) (*UserName, error){
+
+	if(value == nil){
+		return nil, errors.New("ユーザ名が入力されていません")
 	}
+
+	if len(*value)< 3{
+		errorMessage := fmt.Sprintf("ユーザ名は3文字以上です %s", *value)
+		return nil, errors.New(errorMessage)
+	}
+
+	userName := &UserName{ value: value }
+
+	return userName, nil
 }
--- go_ddd/ch02/user_name_test.go
+++ go_ddd/ch02/user_name_test.go
@@ -4,10 +4,25 @@ import (
 	"testing"
 )
 
-
 func TestNewUserName(t *testing.T) {
-	userName := NewUserName("kashiwara")
-	if userName.value != "kashiwara" {
+	inputName := "kashiwara"
+	userName, _ := NewUserName(&inputName)
+	if *userName.value != "kashiwara" {
 		t.Errorf("kashiwaraがvalueに入っていない")
+	}
+}
+
+func TestNameIsNil(t *testing.T) {
+	_, err := NewUserName(nil)
+	if err.Error() != "ユーザ名が入力されていません" {
+		t.Errorf("意図していないエラーメッセージ: %s", err.Error())
+	}
+}
+
+func TestNameIsLessThanTree(t *testing.T) {
+	inputName := "k"
+	_, err := NewUserName(&inputName)
+	if err.Error() != "ユーザ名は3文字以上です k" {
+		t.Errorf("意図していないエラーメッセージ: %s", err.Error())
 	}
 }
